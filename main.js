ef0e08b1d6faa310be9403585132cfa5d216d8f3
IPORipo <roma.sumbadze@gmail.com>
2024-02-20 16:15:43 +0400

    updates

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+function underscoreToCamelCase(inputString){
+    return inputString.replace(/_./g, (match) => match.charAt(1).toUpperCase())
+  }
+  
+import data1 from "./sta-capability-management-configs_19-02-2024.json"
+import  data2 from "./sta-data-dictionary-configs_20-02-2024.json"
+import properties from "./properties.json"
+
+const final = JSON.stringify(data1) + JSON.stringify(data2)
+
+const res = properties.filter((x) => {
+  return final.includes(underscoreToCamelCase(x))
+})
+
+res.forEach((x) => console.log(x))
+
+document.querySelector('#app').innerHTML = `  tadam
+`;
