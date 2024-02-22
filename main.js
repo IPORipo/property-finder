771b37ac8f9a821801d2548ce2ecb3f2bcaad9a3
IPORipo <roma.sumbadze@gmail.com>
2024-02-22 18:39:46 +0400

    add more details

--- main.js
+++ main.js
@@ -1,19 +1,71 @@
-
-
-
-function underscoreToCamelCase(inputString){
-    return inputString.replace(/_./g, (match) => match.charAt(1).toUpperCase())
-  }
-  
-import data1 from "./sta-capability-management-configs_19-02-2024.json"
-import  data2 from "./sta-data-dictionary-configs_20-02-2024.json"
+function underscoreToCamelCase(inputString) {
+  return inputString.replace(/_./g, (match) => match.charAt(1).toUpperCase())
+}
+function find(where,what){
+  return new RegExp("\\b" + what + "\\b").test(where)
+  // return where.includes(what);
+}
 import properties from "./properties.json"
+import data1 from "./sta-capability-management-configs_19-02-2024.json"
+import data2 from "./sta-data-dictionary-configs_20-02-2024.json"
 
 const final = JSON.stringify(data1) + JSON.stringify(data2)
 
-let res = properties.filter((x) => {
-  return final.includes(underscoreToCamelCase(x))
+const transformedProps = properties.map((x) => underscoreToCamelCase(x))
+let usedProperties = transformedProps.filter((x) => {
+  return find(final,x)
 })
 
-res = res.map(x=>underscoreToCamelCase(x))
-document.querySelector('#app').innerHTML = res.join("<br>")
+let res = `
+<strong>Found ${usedProperties.length || 0} used properties out of ${properties.length || 0} :</strong>
+<br>
+<em>${usedProperties.join("<br>")}</em>
+
+<h2>See details of categories and corresponding config Id's</h2>
+`;
+
+res += usedProperties.reduce((acc, curProp) => {
+  let propInfo = ''
+  propInfo += findInCategory("tableFields", curProp,'Table Fields')
+  propInfo += findInCategory("tables", curProp)
+  propInfo += findInCategory("grids", curProp)
+  propInfo += findInCategory("charts", curProp)
+  propInfo += findInCategory("filters", curProp)
+  propInfo += findInCategory("capabilities", curProp)
+
+  if (propInfo) {
+    acc += `<div><h2>${curProp} is used in </h2><div class="categories">${propInfo}</div></div><hr>`
+  }
+  return acc
+}, '')
+
+
+
+document.querySelector("#app").innerHTML = res
+
+function findInCategory(category, prop,categoryName) {
+  const res = data1[category]?.reduce((acc, cur) => {
+    if (find(JSON.stringify(cur),prop))
+      acc += `${cur.id} <br>`
+    return acc;
+  }, '')
+  return res ? `<div class="category"><em><h5>${(categoryName || category).toUpperCase()}:</h5></em>${res}</div>`
+    : ''
+}
+
+// function underscoreToCamelCase(inputString){
+//   return inputString.replace(/_./g, (match) => match.charAt(1).toUpperCase())
+// }
+
+// import data1 from "./sta-capability-management-configs_19-02-2024.json"
+// import  data2 from "./sta-data-dictionary-configs_20-02-2024.json"
+// import properties from "./properties.json"
+
+// const final = JSON.stringify(data1) + JSON.stringify(data2)
+
+// let res = properties.filter((x) => {
+// return final.includes(underscoreToCamelCase(x))
+// })
+
+// res = res.map(x=>underscoreToCamelCase(x))
+// document.querySelector('#app').innerHTML = res.join("<br>")
